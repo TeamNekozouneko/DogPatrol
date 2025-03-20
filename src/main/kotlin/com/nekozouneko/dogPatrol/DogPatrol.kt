e95879883471abe41616993f2760f2bd5878b170
るんく君 <91005690+RunqRun@users.noreply.github.com>
2025-03-20 16:15:21 +0900

    Project Init

---
+++ src/main/kotlin/com/nekozouneko/dogPatrol/DogPatrol.kt
@@ -0,0 +1,14 @@
+package com.nekozouneko.dogPatrol
+
+import net.md_5.bungee.api.plugin.Plugin
+
+class DogPatrol : Plugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
