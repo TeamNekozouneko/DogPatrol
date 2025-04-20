65bcbec654dd0fd92467e7ad617dd18b80aec11d
るんく君 <91005690+RunqRun@users.noreply.github.com>
2025-04-20 17:11:37 +0900

    add: サーバーの除外設定ができるように

--- src/main/kotlin/com/nekozouneko/dogPatrol/listener/ChatEvent.kt
+++ src/main/kotlin/com/nekozouneko/dogPatrol/listener/ChatEvent.kt
@@ -1,15 +1,25 @@
 package com.nekozouneko.dogPatrol.listener
 
 import com.nekozouneko.dogPatrol.DogPatrol
+import net.md_5.bungee.api.connection.ProxiedPlayer
 import net.md_5.bungee.api.event.ChatEvent
 import net.md_5.bungee.api.plugin.Listener
 import net.md_5.bungee.event.EventHandler
 
 class ChatEvent : Listener {
     private val checkManager = DogPatrol.getCheckManager()
+    private val configurationManager = DogPatrol.getConfigurationManager()
     @EventHandler
     fun onChat(e: ChatEvent){
         if(e.isCommand || e.isProxyCommand) return
+
+        //Exclude Servers
+        val player = e.sender as ProxiedPlayer
+        val configFile = configurationManager.getConfig()
+        val excludeServers = configFile.getStringList("exclude_servers")
+        if(excludeServers.contains(player.server.info.name)) return
+
+        //Handle
         checkManager.check(e)
     }
 }
